21ee6ae8d5889d6646f11d0d98adb7e969631068
VijayLingamaneni <84323645+VijayLingamaneni@users.noreply.github.com>
2022-04-02 13:59:56 +0530

    Add files via upload

---
+++ screens/login/login/login.js
@@ -0,0 +1,131 @@
+/* eslint-disable prettier/prettier */
+
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  ToastAndroid,
+} from "react-native";
+import { Text } from "react-native-paper";
+import Buttons from "../../../component/atoms/button/button";
+import Heading from "../../../component/atoms/text/heading";
+import InputLable from "../../../component/molecules/inputLabel";
+import FlexBoxTowContainer from "../../../containers/flexboxwithGap";
+import ScreenContainer from "../../../containers/screenContainer";
+import labelStyle from "../../../styles/label/label";
+import viewsStyle from "../../../styles/view/view";
+import color from "../../../themes/color/color";
+import margin from "../../../themes/commonStyle";
+import axios from "axios";
+import { useNavigation } from "@react-navigation/native";
+import * as SecureStore from "expo-secure-store";
+
+function Login({ navigation }) {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  axios.defaults.withCredentials = true;
+  const navigations = useNavigation();
+
+  function pressBtn() {
+    if (email == "" || password == "") {
+      ToastAndroid.show("Please fill all fields !", ToastAndroid.SHORT);
+      return;
+    }
+
+    axios
+      .post(
+        "https://ecommerce120.herokuapp.com/api/v1/login",
+        {
+          email: email,
+          pass: password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            withCredentials: true,
+            credentials: "include",
+          },
+        }
+      )
+      .then(async (res) => {
+        console.log(res);
+        console.log(await SecureStore.getItemAsync("login"));
+        await SecureStore.setItemAsync("login", 'true');
+        console.log(await SecureStore.getItemAsync("login"));
+        navigations.navigate("Home");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <ScreenContainer>
+      <View style={style.upperView}>
+        <View style={viewsStyle.headingAndInputView}>
+          <Heading heading="Welcome back to Mega Mall" />
+          <Text style={labelStyle.smallText}>Sign in your account here</Text>
+        </View>
+        <View>
+          <InputLable
+            text="Email"
+            placeholder="Type your email"
+            callBack={setEmail}
+          />
+          <InputLable
+            text="Password"
+            placeholder="Type your password"
+            type="password"
+            callBack={setPassword}
+          />
+        </View>
+      </View>
+
+      <View style={style.lowerView}>
+        <KeyboardAvoidingView
+          style={style.lowerView}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 50}
+          enabled={Platform.OS === "ios"}
+        >
+          <View style={style.buttonContainer}>
+            <Buttons disabled={true} text="Sign in" callBack={pressBtn} />
+          </View>
+          <View style={style.lowerButtons}>
+            <FlexBoxTowContainer
+              leftItem={<Text></Text>}
+              rightItem={
+                <Text
+                  onPress={() => navigation.navigate("Register")}
+                  style={{ color: color.primaryBlue }}
+                >
+                  Sign Up
+                </Text>
+              }
+            />
+          </View>
+        </KeyboardAvoidingView>
+      </View>
+    </ScreenContainer>
+  );
+}
+
+const style = StyleSheet.create({
+  upperView: {
+    marginBottom: margin.buttonAndInputMargin,
+  },
+  lowerButtons: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 7,
+  },
+  lowerView: {
+    flex: 1,
+  },
+});
+
+export default Login;
---
+++ screens/login/register/register.js
@@ -0,0 +1,128 @@
+/* eslint-disable prettier/prettier */
+
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text } from "react-native-paper";
+import axios from "axios";
+import { ScrollView } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+import Buttons from "../../../component/atoms/button/button";
+import Heading from "../../../component/atoms/text/heading";
+import InputLable from "../../../component/molecules/inputLabel";
+import ScreenContainer from "../../../containers/screenContainer";
+import labelStyle from "../../../styles/label/label";
+import viewsStyle from "../../../styles/view/view";
+import color from "../../../themes/color/color";
+import margin from "../../../themes/commonStyle";
+import * as SecureStore from "expo-secure-store";
+
+function Register({ navigation }) {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [name, setName] = useState("");
+
+  axios.defaults.withCredentials = true;
+  const navigations = useNavigation();
+
+  function pressBtn() {
+    if (email == "" || password == "") {
+      ToastAndroid.show("Please fill all fields !", ToastAndroid.SHORT);
+      return;
+    }
+
+    axios
+      .post(
+        "https://ecommerce120.herokuapp.com/api/v1/register",
+        { name: name, phone: phone, email: email, pass: password },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            withCredentials: true,
+            credentials: "include",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        SecureStore.setItemAsync("login", true);
+        navigations.navigate("Home");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <ScreenContainer>
+    <ScrollView>
+      <View style={style.upperView}>
+        <View style={viewsStyle.headingAndInputView}>
+          <Heading heading="Register" />
+          <Text style={labelStyle.smallText}>
+            Enter Email and password for register{" "}
+          </Text>
+        </View>
+        <View>
+          <InputLable
+            callBack={setEmail}
+            text="Email"
+            placeholder="Type your email"
+          />
+          <InputLable
+            callBack={setName}
+            text="Name"
+            placeholder="Type your name"
+          />
+          <InputLable
+            callBack={setPhone}
+            text="Phone Number"
+            placeholder="Type your phone number"
+          />
+          <InputLable
+            callBack={setAddress}
+            text="Address"
+            placeholder="Type your address"
+          />
+          <InputLable
+            text="Password"
+            callBack={setPassword}
+            placeholder="Type your password"
+            type="password"
+          />
+        </View>
+      </View>
+
+      <View style={style.lowerView}>
+        <View style={style.buttonContainer}>
+          <Buttons disabled={true} text="Sign up" callBack={pressBtn} />
+        </View>
+        <View style={style.lowerButtons}>
+          <Text style={labelStyle.smallText}>
+            Have an Account?{" "}
+            <Text
+              onPress={() => navigation.navigate("Login")}
+              style={{ color: color.primaryBlue }}
+            >
+              Sign In
+            </Text>
+          </Text>
+        </View>
+      </View>
+      </ScrollView>
+    </ScreenContainer>
+  );
+}
+
+const style = StyleSheet.create({
+  upperView: {
+    marginBottom: margin.buttonAndInputMargin,
+  },
+  lowerButtons: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
+
+export default Register;
